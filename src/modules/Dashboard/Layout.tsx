89393bf3d7899ff340482b81ad0fe2b0fc5141af
MD. ASIF BIN ABEDIN <asif.abedin@selisegroup.com>
2025-06-30 17:03:45 +0600

    v2

--- src/modules/Dashboard/Layout.tsx
+++ src/modules/Dashboard/Layout.tsx
@@ -3,7 +3,7 @@ import Dashboard from "../../pages/Dashboard/Dashboard.page";
 const Layout = () => {
   return (
     <div>
-      This is Dashboard Layout
+      This is Dashboard Layout v2
       <Dashboard />
     </div>
   );
